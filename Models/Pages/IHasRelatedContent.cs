c770a73c3b95dc4889af1774a2540809053b1422
Isak Svarre <Isak.svarre@gmail.com>
2025-06-04 12:02:39 +0200

    epi-alloy-mvc template

---
+++ Models/Pages/IHasRelatedContent.cs
@@ -0,0 +1,6 @@
+namespace ApprenticeshipExam.Models.Pages;
+
+public interface IHasRelatedContent
+{
+    ContentArea RelatedContentArea { get; }
+}
---
+++ Models/Pages/ISearchPage.cs
@@ -0,0 +1,8 @@
+namespace ApprenticeshipExam.Models.Pages;
+
+/// <summary>
+/// Marker interface for search implementation
+/// </summary>
+public interface ISearchPage
+{
+}
